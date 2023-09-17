117cd2005ed57e8f1430966249d8d0e2b32b598d
Izzie Vazquez <izzievazquez@gmail.com>
2023-09-16 22:21:23 -0600

    Completed Prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,78 @@
+/*
+File: Program.cs
+Author: Izzie Vazquez
+Overview: Write a program that determines the letter grade for a course according to the following scale:
+    A >= 90
+    B >= 80
+    C >= 70
+    D >= 60
+    F < 60
+Assignment: Start by completing the core requirements, then when that part is complete, if you have time, see if 
+you can complete some of the stretch challenges as well. Please work through the requirements in order rather than 
+jumping ahead to more complicated steps, to ensure that everyone is following the concepts.
+Core Requirements: 
+    1. Ask the user for their grade percentage, then write a series of if-elif-else statements to print out the 
+    appropriate letter grade. (At this point, you'll have a separate print statement for each grade letter in the 
+    appropriate block.)
+    2. Assume that you must have at least a 70 to pass the class. After determining the letter grade and printing 
+    it out. Add a separate if statement to determine if the user passed the course, and if so display a message to 
+    congratulate them. If not, display a different message to encourage them for next time.
+    3. Change your code from the first part, so that instead of printing the letter grade in the body of each if, 
+    elif, or else block, instead create a new variable called letter and then in each block, set this variable to 
+    the appropriate value. Finally, after the whole series of if-elif-else statements, have a single print statement
+    that prints the letter grade once.
+Stretch Challenge:
+    1. Add to your code the ability to include a "+" or "-" next to the letter grade, such as B+ or A-. For each 
+    grade, you'll know it is a "+" if the last digit is >= 7. You'll know it is a minus if the last digit is < 3 
+    and otherwise it has no sign.
+    After your logic to determine the grade letter, add another section to determine the sign. Save this sign into 
+    a variable. Then, display both the grade letter and the sign in one print statement.
+    2. Recognize that there is no A+ grade, only A and A-. Add some additional logic to your program to detect this 
+    case and handle it correctly.
+    3. Similarly, recognize that there is no F+ or F- grades, only F. Add additional logic to your program to detect 
+    these cases and handle them correctly.
+*/
 using System;
+class Program {
+    static void Main(string[] args) {
+        // Prompts user to enter grade percentage.
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+        // Initializes letterGrade
+        string letterGrade = "F";
+
+        // Determines which letter corresponds to the inputted grade percentage and assigns it to letterGrade.
+        if (gradePercentage >= 90) {
+            letterGrade = "A";
+        } else if (gradePercentage >= 80) {
+            letterGrade = "B";
+        } else if (gradePercentage >= 70) {
+            letterGrade = "C";
+        } else if (gradePercentage >= 60) {
+            letterGrade = "D";
+        }
+
+        // Stores the last digit of gradePercentage as an int.
+        int lastDigit = gradePercentage % 10;
+
+        // Determines whether letterGrade should recieve a +, -, or neither.
+        if (gradePercentage >= 60 && gradePercentage < 97)
+            if (lastDigit >= 7) {
+                letterGrade += "+";
+            } else if (lastDigit < 3) {
+                letterGrade += "-";
+            }
+
+        // Prints the received letter grade to the console.
+        Console.WriteLine($"You have received a(n) {letterGrade} in this class.");
+
+        // Determines whether or not the gradePercentage is a passing or failing grade.
+        if (gradePercentage >= 70) {
+            Console.WriteLine("Congratulations! You have passed this course.");
+        } else {
+            Console.WriteLine("You have not passed this course. Better luck next semester!");
+        }
     }
 }
