085e0c200594b2b4e9d01880e2a4cf08ac08164f
Izzie Vazquez <izzievazquez@gmail.com>
2024-09-25 13:16:31 -0600

    Editing github settings.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,23 @@
+/* 
+   Name: Izzie Vazquez
+   Assignment Name: Learning Activity - C# Prep 3
+   Assignment Description: In the Guess My Number game the computer picks a magic number, and then the user tries to guess it. After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.
+   Reflection:
+   Time taken:
+*/
 using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    static void Main(string[] args) {
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        while (guess != magicNumber) {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        } 
     }
 }
