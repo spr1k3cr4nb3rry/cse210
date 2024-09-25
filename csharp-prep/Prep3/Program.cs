f0694d863ab7149b4931744de267225161e56e21
Izzie Vazquez <izzievazquez@gmail.com>
2024-09-25 14:59:18 -0600

    Completed C# Prep 3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,22 +2,41 @@
    Name: Izzie Vazquez
    Assignment Name: Learning Activity - C# Prep 3
    Assignment Description: In the Guess My Number game the computer picks a magic number, and then the user tries to guess it. After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.
-   Reflection:
-   Time taken:
 */
 using System;
 
 class Program
 {
-    static void Main(string[] args) {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+    static void Main(string[] args) // The entry point for the number-guessing game.
+    {
+        string continueGame = "y";
+        while (continueGame == "y")
+        {
+            Random randomGen = new Random();
+            int magicNumber = randomGen.Next(1, 101);
 
-        int guess = -1;
+            int guess = -1;
+            int guessCount = 0;
 
-        while (guess != magicNumber) {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
-        } 
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher!");
+                }
+            }
+
+            Console.WriteLine($"You guessed the number in {guessCount} guesses! The number was {magicNumber}.");
+            Console.Write("Play again? (y/n) ");
+            continueGame = Console.ReadLine();
+        }
     }
 }
