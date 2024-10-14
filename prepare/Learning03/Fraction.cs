9d9d13718a74f2c22c267e2155f0f9cef6f7f736
Izzie Vazquez <izzievazquez@gmail.com>
2024-10-14 15:34:30 -0600

    Learning 03 completed.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction() 
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber) 
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double) _numerator / _denominator;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract1 = new(); // No values given.
+        Fraction fract2 = new(5); // Whole number given.
+        Fraction fract3 = new(3, 4); // Numerator and denominator given.
+
+        Console.WriteLine(fract1.GetFractionString());
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract3.GetFractionString());
+
+        Console.WriteLine();
+
+        Console.WriteLine(fract1.GetDecimalValue());
+        Console.WriteLine(fract2.GetDecimalValue());
+        Console.WriteLine(fract3.GetDecimalValue());
     }
 }
