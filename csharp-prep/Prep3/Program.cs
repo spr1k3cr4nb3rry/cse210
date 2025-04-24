efae5b2c2d32f9ca647aaddf453c459b5103b528
Izzie Vazquez <izzievazquez@gmail.com>
2025-04-24 10:35:36 -0600

    W02 Prove: Developer—C# Programming Parts 3–5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,17 +1,11 @@
-/* 
-   Name: Izzie Vazquez
-   Assignment Name: Learning Activity - C# Prep 3
-   Assignment Description: In the Guess My Number game the computer picks a magic number, and then the user tries to guess it. After each guess, the computer tells the user to guess "higher" or "lower" until they guess the magic number.
-*/
 using System;
 
 class Program
 {
-    static void Main(string[] args) // The entry point for the number-guessing game.
+    static void Main(string[] args)
     {
-        string continueGame = "y";
-        while (continueGame == "y")
-        {
+        bool continueGame = true;
+        while (continueGame) {
             Random randomGen = new Random();
             int magicNumber = randomGen.Next(1, 101);
 
@@ -24,19 +18,21 @@ class Program
                 guess = int.Parse(Console.ReadLine());
                 guessCount++;
 
-                if (guess > magicNumber)
-                {
+                if (guess > magicNumber) {
                     Console.WriteLine("Lower!");
                 }
-                else if (guess < magicNumber)
-                {
+                else {
                     Console.WriteLine("Higher!");
                 }
             }
 
             Console.WriteLine($"You guessed the number in {guessCount} guesses! The number was {magicNumber}.");
+
             Console.Write("Play again? (y/n) ");
-            continueGame = Console.ReadLine();
+            string answer = Console.ReadLine();
+            if (answer == "n") {
+                continueGame = false;
+            }
         }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,14 +1,43 @@
-/* 
-   Name: Izzie Vazquez
-   Assignment Name: Learning Activity - C# Prep 4
-   Assignment Description: 
-*/
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        int number = -1;
+        int maximum = 0;
+        int minimum = 1000;
+
+        while (number != 0) {
+            Console.Write("Enter number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number != 0) {
+                if (number > maximum) { maximum = number; }
+                if (number < minimum && number > 0) { minimum = number; }
+                numbers.Add(number);
+            }
+        }
+
+        // Sum of the numbers in the list.
+        int sum = 0;
+        foreach (int num in numbers) { sum += num;}
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Average of the numbers in the list.
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {maximum}");
+        Console.WriteLine($"The smallest postive number is: {minimum}");
+
+        Console.WriteLine("The sorted list is: ");
+        numbers.Sort();
+        foreach (int num in numbers) {
+            Console.WriteLine(num);
+        }
     }
 }
