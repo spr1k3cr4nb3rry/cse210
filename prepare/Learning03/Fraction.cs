c6b6f5255f016ac65fc94611ed6e7cff0aaf6d15
Izzie Vazquez <izzievazquez@gmail.com>
2025-05-22 10:29:29 -0600

    W05 Prepare: Learning Activity

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,35 +1,29 @@
 using System;
 
-class Fraction
-{
+class Fraction {
     private int _numerator; 
     private int _denominator;
 
-    public Fraction() 
-    {
+    public Fraction() {
         _numerator = 1;
         _denominator = 1;
     }
 
-    public Fraction(int wholeNumber) 
-    {
+    public Fraction(int wholeNumber) {
         _numerator = wholeNumber;
         _denominator = 1;
     }
 
-    public Fraction(int numerator, int denominator)
-    {
+    public Fraction(int numerator, int denominator) {
         _numerator = numerator;
         _denominator = denominator;
     }
 
-    public string GetFractionString()
-    {
+    public string GetFractionString() {
         return $"{_numerator}/{_denominator}";
     }
 
-    public double GetDecimalValue()
-    {
+    public double GetDecimalValue() {
         return (double) _numerator / _denominator;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,7 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         Fraction fract1 = new(); // No values given.
         Fraction fract2 = new(5); // Whole number given.
         Fraction fract3 = new(3, 4); // Numerator and denominator given.
